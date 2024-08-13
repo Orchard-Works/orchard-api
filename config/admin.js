b5e99e133541aa4ccae8d42360773368c2807402
Dean Tawonezvi <deantawonezvi1@gmail.com>
2024-08-13 08:26:42 +0200

    Config test

--- config/admin.js
+++ config/admin.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: "tobemodified",
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: "tobemodified",
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: "tobemodified",
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: true,
+    promoteEE: true,
   },
 });
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+module.exports = () => ({
+  "netlify-deployments": {
+    enabled: true,
+    config: {
+      accessToken: "nfp_s8xyn233gEmcX16crsDyDTXVvcj4qLqs157e",
+
+      sites:[
+        {
+          name: 'orchard-works-api',
+          id: "30102000-d4d0-4f59-9e41-1f94cbcead13",
+          buildHook:"https://api.netlify.com/build_hooks/66baf05d2f3b1aab05e3fe34",
+        }
+      ]
+    },
+  },
+});
+

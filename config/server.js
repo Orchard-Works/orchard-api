9a1cf2860a431ac2b579cbec1a4e9fbeb2e30bd7
Dean Tawonezvi <deantawonezvi1@gmail.com>
2024-08-19 17:37:58 +0200

    Updates host URL

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('STRAPI_ADMIN_URL', 'https://orchard-api-211l.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },

bcc997cf80a6f6b2fcce3ea70f44ffd4ef685d6b
Dean Tawonezvi <deantawonezvi1@gmail.com>
2024-08-19 17:14:49 +0200

    Updates host URL

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'https://orchard-api-211l.onrender.com/'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),

aa5b427a9661e6a9eaf5a18c04b5b81263910a61
Dean Tawonezvi <deantawonezvi1@gmail.com>
2024-09-22 15:11:25 +0200

    Variables update

--- config/database.js
+++ config/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => {
   const connections = {
     postgres: {
       connection: {
-        host: env('DATABASE_HOST', 'aws-0-eu-central-1.pooler.supabase.com'),
-        port: env.int('DATABASE_PORT', 6543),
-        database: env('DATABASE_NAME', 'postgres'),
-        user: env('DATABASE_USERNAME', 'postgres.qpiztsshbfbynliwozjs'),
-        password: env('DATABASE_PASSWORD', 'SkGO5Ku6bF70b7S7'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: env.bool('DATABASE_SSL', false),
         schema: env('DATABASE_SCHEMA', 'public'),
       },

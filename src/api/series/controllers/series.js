09775d63373de92bb544e99b1c78ea84576d9bda
Dean Tawonezvi <deantawonezvi1@gmail.com>
2024-10-14 17:37:44 +0200

    Removes reordering

--- src/api/series/controllers/series.js
+++ src/api/series/controllers/series.js
@@ -1,53 +1,9 @@
 'use strict';
 
+/**
+ * series controller
+ */
+
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::series.series', ({ strapi }) => ({
-  async findOne(ctx) {
-    const { id } = ctx.params;
-    const { query } = ctx;
-
-    const entity = await strapi.service('api::series.series').findOne(id, {
-      ...query,
-      populate: ['episodes'],
-    });
-
-    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-
-    // Sort episodes by order
-    if (sanitizedEntity.episodes) {
-      sanitizedEntity.episodes.sort((a, b) => a.order - b.order);
-    }
-
-    return this.transformResponse(sanitizedEntity);
-  },
-
-  async updateEpisodeOrder(ctx) {
-    const { id } = ctx.params;
-    const { episodeOrder } = ctx.request.body;
-
-    const series = await strapi.service('api::series.series').findOne(id, {
-      populate: ['episodes'],
-    });
-
-    if (!series) {
-      return ctx.notFound('Series not found');
-    }
-
-    // Update the order of each episode
-    for (const orderUpdate of episodeOrder) {
-      await strapi.service('api::episode.episode').update(orderUpdate.id, {
-        data: { order: orderUpdate.order },
-      });
-    }
-
-    // Fetch the updated series with sorted episodes
-    const updatedSeries = await strapi.service('api::series.series').findOne(id, {
-      populate: ['episodes'],
-    });
-
-    updatedSeries.episodes.sort((a, b) => a.order - b.order);
-
-    return this.transformResponse(updatedSeries);
-  },
-}));
+module.exports = createCoreController('api::series.series');
--- src/api/series/routes/series.js
+++ src/api/series/routes/series.js
@@ -1,61 +1,9 @@
-// src/api/series/routes/series.js
 'use strict';
 
-module.exports = {
-  routes: [
-    {
-      method: 'GET',
-      path: '/series',
-      handler: 'series.find',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'GET',
-      path: '/series/:id',
-      handler: 'series.findOne',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'POST',
-      path: '/series',
-      handler: 'series.create',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'PUT',
-      path: '/series/:id',
-      handler: 'series.update',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'DELETE',
-      path: '/series/:id',
-      handler: 'series.delete',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'PUT',
-      path: '/series/:id/episode-order',
-      handler: 'series.updateEpisodeOrder',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-  ],
-};
+/**
+ * series router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::series.series');
